60fe30b42f7d4bea25088c5e3527e795e6788ca2
Ryzen <kbayu788@gmail.com>
2024-06-29 17:31:28 +0700

    update_titan.sh

--- update_titan.sh
+++ update_titan.sh
@@ -1,45 +1,50 @@
 #!/bin/bash
 
-# Function to show loading message
+# Function to show loading message with yellow color
 show_loading() {
     pid=$!
     spin='-\|/'
     i=0
     while kill -0 $pid 2>/dev/null; do
         i=$(( (i+1) %4 ))
-        printf "\r%s" "${spin:$i:1}"
+        printf "\r\033[0;33m%s\033[0m" "${spin:$i:1}"
         sleep .1
     done
     printf "\r"
 }
 
-echo "Memulai proses pembaruan..."
+# Function to print success message in light blue
+print_success() {
+    printf "\033[1;34m%s\033[0m\n" "$1"
+}
+
+echo "Starting the update process..."
 
 # Stop the existing titan-edge service
-echo -n "Menghentikan layanan titan-edge..."
+echo -n "Stopping the titan-edge service..."
 sudo systemctl stop titand.service & show_loading
-echo "Selesai."
+print_success "Done."
 
 # Remove existing titan-edge files
-echo -n "Menghapus file titan-edge yang lama..."
+echo -n "Removing old titan-edge files..."
 sudo rm -rf /usr/local/titan & show_loading
-echo "Selesai."
+print_success "Done."
 
 # Download and install the patch
-echo -n "Mengunduh paket pembaruan..."
+echo -n "Downloading the update package..."
 wget https://github.com/Titannet-dao/titan-node/releases/download/v0.1.19/titan-l2edge_v0.1.19_patch_linux_amd64.tar.gz & show_loading
-echo "Selesai."
+print_success "Done."
 
-echo -n "Mengekstrak paket pembaruan..."
+echo -n "Extracting the update package..."
 sudo tar -xf titan-l2edge_v0.1.19_patch_linux_amd64.tar.gz -C /usr/local & show_loading
 rm titan-l2edge_v0.1.19_patch_linux_amd64.tar.gz
-echo "Selesai."
+print_success "Done."
 
 # Move the new installation to the correct location
 if [ -d "/usr/local/titan-edge_v0.1.19_89e53b6_linux_amd64" ]; then
-    echo -n "Memindahkan instalasi baru ke lokasi yang benar..."
+    echo -n "Moving the new installation to the correct location..."
     sudo mv /usr/local/titan-edge_v0.1.19_89e53b6_linux_amd64 /usr/local/titan & show_loading
-    echo "Selesai."
+    print_success "Done."
 else
     echo "Error: Directory /usr/local/titan-edge_v0.1.19_89e53b6_linux_amd64 does not exist."
     exit 1
@@ -47,16 +52,16 @@ fi
 
 # Copy necessary files
 if [ -f "/usr/local/titan/libgoworkerd.so" ]; then
-    echo -n "Menyalin file yang diperlukan..."
+    echo -n "Copying necessary files..."
     sudo cp /usr/local/titan/libgoworkerd.so /usr/lib/libgoworkerd.so & show_loading
-    echo "Selesai."
+    print_success "Done."
 else
     echo "Error: File /usr/local/titan/libgoworkerd.so does not exist."
     exit 1
 fi
 
 # Add titan to PATH and LD_LIBRARY_PATH in .bash_profile
-echo -n "Menambahkan titan ke PATH dan LD_LIBRARY_PATH di .bash_profile..."
+echo -n "Adding titan to PATH and LD_LIBRARY_PATH in .bash_profile..."
 content="
 export PATH=\$PATH:/usr/local/titan
 export LD_LIBRARY_PATH=\$LD_LIBRARY_PATH:/usr/lib
@@ -68,43 +73,45 @@ if [ ! -f ~/.bash_profile ]; then
 else
   echo "$content" >> ~/.bash_profile
 fi
-echo "Selesai."
+print_success "Done."
 
 # Source the .bash_profile to update the current shell environment
 source ~/.bash_profile
 
 # Run titan-edge daemon in the background
-echo -n "Menjalankan daemon titan-edge..."
+echo -n "Running the titan-edge daemon..."
 (titan-edge daemon start --init --url https://cassini-locator.titannet.io:5000/rpc/v0 &) &
 daemon_pid=$!
 show_loading
-echo "PID daemon titan-edge: $daemon_pid"
+print_success "Daemon PID: $daemon_pid"
 
 # Wait 15 seconds to ensure that the daemon has started successfully
 sleep 15
 
 # Run titan-edge bind in the background
-echo -n "Menjalankan titan-edge bind..."
+echo -n "Running titan-edge bind..."
 (titan-edge bind --hash="$hash_value" https://api-test1.container1.titannet.io/api/v2/device/binding &) &
 bind_pid=$!
 show_loading
-echo "PID titan-edge bind: $bind_pid"
+print_success "Bind PID: $bind_pid"
 
 # Wait for the binding process to finish
 wait $bind_pid
 
 # Update systemd
-echo -n "Memperbarui systemd..."
+echo -n "Updating systemd..."
 sudo systemctl daemon-reload & show_loading
-echo "Selesai."
+print_success "Done."
 
 # Restart titand.service
-echo -n "Memulai ulang layanan titand.service..."
+echo -n "Restarting the titand.service..."
 sudo systemctl restart titand.service & show_loading
-echo "Selesai."
+print_success "Done."
 
 # Check the status and show the configuration and info
-echo -n "Memeriksa status layanan..."
+echo -n "Checking the service status..."
 sleep 8
 sudo systemctl status titand.service && titan-edge config show && titan-edge info
-echo "########################## UPDATE COMPLETED ##########################"
+
+print_success "########################## UPDATE COMPLETED ##########################"
+
