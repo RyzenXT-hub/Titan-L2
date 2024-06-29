5c79e619304da8e2abd2cd23e358a69415192ac2
Ryzen <kbayu788@gmail.com>
2024-06-29 16:42:28 +0700

    update_titan.sh

--- update_titan.sh
+++ update_titan.sh
@@ -1,12 +1,21 @@
 #!/bin/bash
 
+# Stop the existing titan-edge service
+sudo systemctl stop titand.service
+
+# Remove existing titan-edge files
+sudo rm -rf /usr/local/titan
+
 # Download and install the patch
 wget https://github.com/Titannet-dao/titan-node/releases/download/v0.1.19/titan-l2edge_v0.1.19_patch_linux_amd64.tar.gz
-sudo tar -xf titan-l2edge_v0.1.19_patch_linux_amd64.tar.gz -C /usr/local/titan
+sudo tar -xf titan-l2edge_v0.1.19_patch_linux_amd64.tar.gz -C /usr/local
 rm titan-l2edge_v0.1.19_patch_linux_amd64.tar.gz
 
-# Stop processes related to titan-edge
-pkill titan-edge
+# Move the new installation to the correct location
+sudo mv /usr/local/titan-l2edge_v0.1.19_patch_linux_amd64 /usr/local/titan
+
+# Copy necessary files
+sudo cp /usr/local/titan/libgoworkerd.so /usr/lib/libgoworkerd.so
 
 # Update systemd
 sudo systemctl daemon-reload
