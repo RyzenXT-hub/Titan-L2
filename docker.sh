0a89bd8d2e334575b5ea6bf9d2a5502c5619e36a
Ryzen <kbayu788@gmail.com>
2024-11-03 16:31:09 +0700

    Update docker.sh

--- docker.sh
+++ docker.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 
-# Function to display colored text
 GREEN='\033[0;32m'
 YELLOW='\033[1;33m'
 NC='\033[0m' # No Color
 
-# Check if the script is run as root
 if [ "$(id -u)" != "0" ]; then
     echo -e "${YELLOW}This script requires root access.${NC}"
     echo -e "${YELLOW}Please enter root mode using 'sudo -i', then rerun this script.${NC}"
@@ -13,16 +11,13 @@ if [ "$(id -u)" != "0" ]; then
     exit 1
 fi
 
-# Prompt the user to enter the identity code
 echo -e "${YELLOW}Please enter your identity code:${NC}"
 read -p "> " id
 
-# Storage and port settings
 storage_gb=50
 start_port=1235
 container_count=5
 
-# Get the list of public IPs
 public_ips=$(curl -s ifconfig.me)
 
 if [ -z "$public_ips" ]; then
@@ -30,7 +25,6 @@ if [ -z "$public_ips" ]; then
     exit 1
 fi
 
-# Check if Docker is installed
 if ! command -v docker &> /dev/null
 then
     echo -e "${GREEN}Docker not detected, installing...${NC}"
@@ -41,11 +35,9 @@ else
     echo -e "${GREEN}Docker is already installed.${NC}"
 fi
 
-# Pull the Docker image
 echo -e "${GREEN}Pulling the Docker image nezha123/titan-edge...${NC}"
 docker pull nezha123/titan-edge
 
-# Set up nodes for each public IP
 current_port=$start_port
 
 for ip in $public_ips; do
@@ -55,26 +47,21 @@ for ip in $public_ips; do
     do
         storage_path="/root/titan_storage_${ip}_${i}"
 
-        # Ensure storage path exists
         mkdir -p "$storage_path"
 
-        # Run the container with restart always policy
         container_id=$(docker run -d --restart always -v "$storage_path:/root/.titanedge/storage" --name "titan_${ip}_${i}" --net=host nezha123/titan-edge)
 
         echo -e "${GREEN}Node titan_${ip}_${i} is running with container ID $container_id${NC}"
 
         sleep 30
 
-        # Modify the config.toml file to set StorageGB and RPC port
         docker exec $container_id bash -c "\
             sed -i 's/^[[:space:]]*#StorageGB = .*/StorageGB = $storage_gb/' /root/.titanedge/config.toml && \
             sed -i 's/^[[:space:]]*#ListenAddress = \"0.0.0.0:1234\"/ListenAddress = \"0.0.0.0:$current_port\"/' /root/.titanedge/config.toml && \
             echo 'Storage for node titan_${ip}_${i} set to $storage_gb GB, Port set to $current_port'"
 
-        # Restart the container for the settings to take effect
         docker restart $container_id
 
-        # Bind the node
         docker exec $container_id bash -c "\
             titan-edge bind --hash=$id https://api-test1.container1.titannet.io/api/v2/device/binding"
         echo -e "${GREEN}Node titan_${ip}_${i} has been bound.${NC}"
